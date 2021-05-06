c9b813ed69a120c5457db6842e289b519265f893
Pietro Delugas <mail@pietrodelugas.it>
2021-05-06 08:37:08 +0200

    removing magmom.dat from references
    we will use total magnetization instead

--- Day-7/exercise3/integral.py
+++ Day-7/exercise3/integral.py
@@ -1,25 +1,35 @@
-import sys
-
-dosfile = sys.argv[1]
-f = open(dosfile,'r')
-lines = f.readlines()
-f.close()
-
-E = []
-pdossumup = []
-pdossumdw = []
-dE = float(lines[2].split()[0])-float(lines[1].split()[0])
-U,D = 0,0
-for line in lines[1:]:
-    e,u,d = line.split()
-    e,u,d = float(e),float(u),float(d)
-    U += u*dE
-    D += d*dE
-    E.append(e)
-    pdossumup.append(U)
-    pdossumdw.append(D)
-
-f = open(dosfile+'.integral','w')
-for e,u,d in zip(E,pdossumup,pdossumdw):
-    f.write(" %.6f  %.10e  %.10e \n"%(e,u,d))
-f.close()
+from sys import argv
+#
+try:
+    input = argv[1]
+except IndexError:
+    print ("please provide as argument the file with the summed pdos: \n")
+    print ("e.g.:  integral.py  foo.dat \n")
+    raise SystemExit 
+#
+with open(input, 'r') as f:
+    next(f)
+    data = [(float(x) for x in l.split()) for l in iter(f)]
+e = [next(_) for _ in data]
+de = [_[1]-_[0] for _ in zip(e[:-1],e[1:])]
+dup = [next(_[0])*_[1] for _ in zip(data[1:],de)]
+ddw = [next(_[0])*_[1] for _ in zip(data[1:],de)]
+#
+def writesums(a,f):
+    i = iter(a)
+    si=0.0
+    sj=0.0
+    a_=(0.,0.,0.)
+    while i:
+        try:
+            a_ = next(i)
+        except StopIteration:
+            break
+        f.write(f"{a_[0]:6.3f}   {si:.8e}  {sj:.8e}  {si-sj:.8e}\n") 
+        yield None 
+        si += a_[1]
+        sj += a_[2]
+#        
+from collections import deque
+with open(f"{input}.int",'w') as f:
+    deque(writesums(zip(e,dup,ddw),f),maxlen=0)
